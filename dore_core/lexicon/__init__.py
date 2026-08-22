329aa9e076a1983fc4457880ee65d97950ad4793
WATCHMAN <westsidewatchca@gmail.com>
2026-08-22 05:55:07 -0400

    feat(dore): initialize lexical resolution package

---
+++ dore_core/lexicon/__init__.py
@@ -0,0 +1,4 @@
+"""Lexical resolution tools for Doré."""
+from .hebrew import HebrewLexicalResolution, parse_oshb_lexical_value, resolve_oshb_lexeme
+
+__all__ = ["HebrewLexicalResolution", "parse_oshb_lexical_value", "resolve_oshb_lexeme"]
