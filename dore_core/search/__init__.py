18d2f17182fdc675fac27a1977ebbc8cdf9cd4b6
WATCHMAN <westsidewatchca@gmail.com>
2026-08-22 19:00:28 -0400

    feat(dore): create Bible Search service package

---
+++ dore_core/search/__init__.py
@@ -0,0 +1,3 @@
+"""Doré Bible Search: stable external access node into Doré Core."""
+from .service import BibleSearchIndex, SearchHit, SearchQuery
+__all__ = ["BibleSearchIndex", "SearchHit", "SearchQuery"]
