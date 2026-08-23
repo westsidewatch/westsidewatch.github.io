a047dad6a4c319ea62c49ca78afee38b270f8a6f
WATCHMAN <westsidewatchca@gmail.com>
2026-08-22 22:08:36 -0400

    feat(dore): expose BW-1 entity reflex

--- dore_core/world/__init__.py
+++ dore_core/world/__init__.py
@@ -1,7 +1,9 @@
 """Doré Biblical World foundation package."""
 from .model import Alias, Attestation, WorldClaim, WorldEntity, WorldValidation, validate_claim, validate_entity
+from .entity_reflex import EntityCandidate, EntityResolution, normalize_mention, resolve_entity
 
 __all__ = [
     'Alias','Attestation','WorldClaim','WorldEntity','WorldValidation',
     'validate_claim','validate_entity',
+    'EntityCandidate','EntityResolution','normalize_mention','resolve_entity',
 ]
