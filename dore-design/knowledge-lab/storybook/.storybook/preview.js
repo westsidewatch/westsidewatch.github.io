2579b05064fb595dfb1644c239fef182e90eb5e2
WATCHMAN <westsidewatchca@gmail.com>
2026-09-02 15:46:53 -0400

    Make accessibility violations fail Storybook browser tests

--- dore-design/knowledge-lab/storybook/.storybook/preview.js
+++ dore-design/knowledge-lab/storybook/.storybook/preview.js
@@ -1,3 +1,7 @@
 export default {
-  parameters: { layout: 'fullscreen', controls: { expanded: true } },
+  parameters: {
+    layout: 'fullscreen',
+    controls: { expanded: true },
+    a11y: { test: 'error' },
+  },
 };
