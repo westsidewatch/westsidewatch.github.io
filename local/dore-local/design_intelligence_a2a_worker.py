805e77927413233594c6e8411b29085073a101ae
WATCHMAN <westsidewatchca@gmail.com>
2026-09-12 20:20:55 -0400

    feat(design): activate v9 bounded repair fallback worker

--- local/dore-local/design_intelligence_a2a_worker.py
+++ local/dore-local/design_intelligence_a2a_worker.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from importlib import import_module
 
-worker = import_module('design_intelligence_a2a_worker_v8')
+worker = import_module('design_intelligence_a2a_worker_v9')
 legacy = worker.legacy
 
 if __name__ == '__main__':
