0eee8f3bd6a2508d9ea2c0066fd9b4b1ce203e0d
WATCHMAN <westsidewatchca@gmail.com>
2026-09-12 20:24:00 -0400

    feat(design): activate v10 executable guardrail worker

--- local/dore-local/design_intelligence_a2a_worker.py
+++ local/dore-local/design_intelligence_a2a_worker.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from importlib import import_module
 
-worker = import_module('design_intelligence_a2a_worker_v9')
+worker = import_module('design_intelligence_a2a_worker_v10')
 legacy = worker.legacy
 
 if __name__ == '__main__':
