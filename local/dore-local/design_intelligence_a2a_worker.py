b73045d3b9a6f6fe4a6258bb3857d62550120a25
WATCHMAN <westsidewatchca@gmail.com>
2026-09-12 20:18:45 -0400

    feat(design): activate v8 learning-consensus worker

--- local/dore-local/design_intelligence_a2a_worker.py
+++ local/dore-local/design_intelligence_a2a_worker.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from importlib import import_module
 
-worker = import_module('design_intelligence_a2a_worker_v7')
+worker = import_module('design_intelligence_a2a_worker_v8')
 legacy = worker.legacy
 
 if __name__ == '__main__':
-    raise SystemExit(legacy.main())
+    raise SystemExit(worker.main())
